e482db1a56c298a8bc55907dfeb03973bee98460
Samar Badriddinov <99094663+SameerBadriddinov@users.noreply.github.com>
2023-08-06 08:14:46 +0500

    #6. Product detail

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,5 +1,44 @@
-const ProductDetailedPage = () => {
-	return <div>ProductDetailedPage</div>;
+import CustomImage from '@/components/image';
+import { notFound } from 'next/navigation';
+
+interface Props {
+	params: {
+		id: string;
+	};
+}
+
+const ProductDetailedPage = async ({ params: { id } }: Props) => {
+	try {
+		const res = await fetch(
+			`https://fakestoreapi.com/products/${id}`
+		);
+		const product = await res.json();
+
+		return (
+			<div className='max-w-5xl mx-auto flex flex-col md:flex-row items-center gap-8 px-4 mt-48 pb-10'>
+				<CustomImage product={product} />
+
+				<div className='divide-2'>
+					<div className='space-y-2 pb-8'>
+						<h1 className='text-2xl md:text-4xl font-bold'>
+							{product.title}
+						</h1>
+						<h2 className='text-gray-500 font-bold text-xl md:text-3xl'>
+							${product.price}
+						</h2>
+					</div>
+
+					<div>
+						<p className='text-xs md:text-sm'>
+							{product.description}
+						</p>
+					</div>
+				</div>
+			</div>
+		);
+	} catch (error) {
+		notFound();
+	}
 };
 
 export default ProductDetailedPage;
--- src/components/image.tsx
+++ src/components/image.tsx
@@ -13,17 +13,34 @@ const CustomImage: FC<Props> = ({ product, fill }) => {
 	const [isLoading, setIsLoading] = useState(true);
 
 	return (
-		<Image
-			src={product.image}
-			alt={product.title}
-			fill
-			className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
-				isLoading
-					? 'scale-110 blur-2xl grayscale'
-					: 'scale-100 blur-0 grayscale-0'
-			}}`}
-			onLoadingComplete={() => setIsLoading(false)}
-		/>
+		<>
+			{fill ? (
+				<Image
+					src={product.image}
+					alt={product.title}
+					fill
+					className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+						isLoading
+							? 'scale-110 blur-2xl grayscale'
+							: 'scale-100 blur-0 grayscale-0'
+					}}`}
+					onLoadingComplete={() => setIsLoading(false)}
+				/>
+			) : (
+				<Image
+					src={product.image}
+					alt={product.title}
+					width={400}
+					height={1000}
+					className={`object-contain duration-700 ease-in-out group-hover:opacity-75 ${
+						isLoading
+							? 'scale-110 blur-2xl grayscale'
+							: 'scale-100 blur-0 grayscale-0'
+					}}`}
+					onLoadingComplete={() => setIsLoading(false)}
+				/>
+			)}
+		</>
 	);
 };
 
